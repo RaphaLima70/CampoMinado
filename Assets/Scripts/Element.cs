cd7e191687feefa91751d9efe7ce3c5b0b39419d
Raphael Albino <raphaelalbino_@hotmail.com>
2020-06-10 02:41:41 -0300

    Subindo o prototipo sem polimento

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    public Sprite[] emptyTexture;
+    public Sprite mineTexture;
+
+    public bool mine;
+
+    void Start()
+    {
+        mine = Random.value < 0.15f;
+
+        int x = (int)transform.localPosition.x;
+        int y = (int)transform.localPosition.y;
+
+        Grid.elements [x,y] = this;
+    }
+
+    public void LoadTexture(int adjacentCount)
+    {
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTexture[adjacentCount];
+        }
+    }
+
+    public bool isCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    void OnMouseUpAsButton()
+    {
+        //print("Clicou");
+        if (mine)
+        {
+            Grid.uncoverElements();
+        }
+        else
+        {
+            int x = (int)transform.localPosition.x;
+            int y = (int)transform.localPosition.y;
+            LoadTexture(Grid.adjacentMines(x, y));
+
+            Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+        }
+    }
+}
